0244c345093da8b960ab8a8c9c2d5bafb59133ec
Enrique1311 <ejspinelli131176@gmail.com>
2024-09-14 18:03:29 -0300

    The sign up page was completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage";
+
 function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={<HomePage />}
+			/>
+			<Route
+				path="/login"
+				element={<LoginPage />}
+			/>
+			<Route
+				path="/signup"
+				element={<SignUpPage />}
+			/>
+		</Routes>
+	);
 }
 
 export default App;
